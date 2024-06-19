8ab25b45187e27779b0f2bcf9607b00b2fd95715
Drakedragoo <163123866+Drakedragoo@users.noreply.github.com>
2024-06-19 08:39:34 -0300

    Add files via upload

--- modelo.py
+++ modelo.py
@@ -1,44 +1,50 @@
 class Programa:
     def __init__(self, nome, ano):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
-        self.__likes = 0
+        self._likes = 0
 
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
     
     @nome.setter
     def nome (self,novo_nome):
-        self.__nome = novo_nome.title()
-    
+        self._nome = novo_nome.title()
+        
     @property
-    def nome(self):
-        return self.__likes
+    def likes(self):
+        return self._likes
+
 
     def dar_likes(self):
-        self.__likes += 1
+        self._likes += 1
+
+    def imprime(self):
+        print(f'{self.nome} - {self.ano} - {self._likes} likes')
 
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome,ano)
         self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
 
 class Serie(Programa):
-    def __init__(self, nome, ano, temporada):
-        self.__nome = nome.title()
+     def __init__(self, nome, ano, temporadas, duracao):
+        super().__init__(nome,ano)
         self.ano = ano
-        self.temporada = temporada
-        self.__likes = 0
-    
-vingadores = Filme('Vingadores - Guerra Infinita', 2018, 160)
+        self.temporadas = temporadas
+        self._likes = 0
+
+vingadores = Filme('Vingadores - guerra infinita', 2018, 160)
 vingadores.dar_likes()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} 'f'- Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
 
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_likes()
 atlanta.dar_likes()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}'f'- Temporadas: {atlanta.temporada} - Likes: {atlanta.likes}')
+
+filmes_e_series = [vingadores,atlanta]
+
+for programa in filmes_e_series:
+    programa.imprime()
